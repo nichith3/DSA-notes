161893615a1b367addfcb7c54361c06c60244fc8
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-01-12 07:52:50 +0530

    Create 53. Maximum_Subarray.java

---
+++ Arrays/53. Maximum_Subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            curr = curr + nums[i];
+            if(curr > max){
+                max = curr;
+            }
+            if(curr < 0){
+                curr = 0;
+            }
+        }
+        return max;
+    }
+}
