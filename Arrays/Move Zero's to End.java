47de02683e076828dd9e3813c91edc496739063e
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-03-20 20:42:56 +0530

    Create Move Zero's to End.java

---
+++ Arrays/Move Zero's to End.java
@@ -0,0 +1,30 @@
+// Approach 1
+// Using extra Array
+// Time : O(n)
+// Space : O(n)
+
+
+// Approach 2
+// Two pointers
+
+public static int[] moveZeros(int n, int []a) {
+
+      int i=0,j=0;
+      while(i<n && j<n){
+        
+          if(a[i]==0 && a[j]!=0){
+          int temp = a[i];
+          a[i] = a[j];
+          a[j] = temp;
+          }
+        
+          if(a[i]!=0){
+              i++;
+          }
+          j++;
+      }
+      return a;
+}
+
+// Time : O(n)
+// Space : O(1)
