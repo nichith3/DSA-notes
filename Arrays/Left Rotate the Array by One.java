ecafcedef4b8e1c77279e5fbd928cd464db56382
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-03-20 19:02:00 +0530

    Create Left Rotate the Array by One.java

---
+++ Arrays/Left Rotate the Array by One.java
@@ -0,0 +1,11 @@
+static int[] rotateArray(int[] arr, int n) {
+        // Write your code here.
+        int i=0;
+        int temp=arr[0];
+        for(i=1; i<n; i++){
+            arr[i-1] = arr[i];
+        }
+
+        arr[n-1] = temp;
+        return arr;
+}
