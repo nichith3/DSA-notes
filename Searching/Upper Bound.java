e161b7c20bdb5c4ea031ed3a99471b203ad3d179
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-04-02 07:03:36 +0530

    Create Upper Bound.java

---
+++ Searching/Upper Bound.java
@@ -0,0 +1,27 @@
+// Upper bound = Small index such that arr[i] > x
+
+// Approach 1
+// Linear search
+
+// Approch 2
+// Binary Search
+
+public class Solution {
+    public static int upperBound(int []arr, int x, int n){
+
+        int low = 0, high = n-1;
+        int ans = n;
+        
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(arr[mid] > x){
+                ans = mid;
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
