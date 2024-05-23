6d63555297ec047b2c0a8fdd12335a251b9b27c4
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-05-23 08:57:40 +0530

    Update 42. Trapping Rain Water.java

--- Arrays/42. Trapping Rain Water.java
+++ Arrays/42. Trapping Rain Water.java
@@ -1,36 +1,121 @@
+//  min (maxleft[i] - maxRight[i]) - arr[i]
+
+// Approach 1
+// Brute force
+
+
 class Solution {
     public int trap(int[] height) {
-        int size = height.length;
-        int left[] = new int[size];
-        int right[] = new int[size];
-        int total = 0,curr;
-        int leftmax=0, rightmax=0;
-
-        for(int i=0; i<size; i++){
-            if(height[i]> leftmax){
-                leftmax = height[i];
-            }
-            left[i] = leftmax;
+        int n = height.length;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            int left = leftMax(height, i);
+            int right = rightMax(height, i);
+            sum += Math.min(left, right) - height[i];
         }
-
-        for(int i=size-1; i>=0; i--){
-            if(height[i]> rightmax){
-                rightmax = height[i];
-            }
-            right[i] = rightmax;
-        }
-
-        for(int i=0; i<size; i++){
-            curr = min(left[i],right[i]) - height[i];
-            total = total + curr;
-        }
-        return total;
+        return sum;
     }
-
-    public int min(int a,int b){
-        if(a<b){
-            return a;
+    public int leftMax(int[] a, int index){
+        int max = a[index];
+        for(int i=index; i>=0; i--){
+            if(max < a[i]){
+                max = a[i];
+            }
         }
-        return b;
+        return max;
+    }
+    public int rightMax(int[] a, int index){
+        int max = a[index];
+        for(int i=index; i<a.length; i++){
+            if(max < a[i]){
+                max = a[i];
+            }
+        }
+        return max;
     }
 }
+
+
+// Time : O(N)
+// Space : O(1)
+
+
+// Approach 2
+// Using prefix, suffix array
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        // Left max - prefix max Array
+        int max=height[0];
+        for(int i=0; i<n; i++){
+            if(height[i] > max){
+                max = height[i];
+            }
+            prefix[i] = max;
+        }
+
+        // Right max - suffix max Array
+        max = height[n-1];
+        for(int i=n-1; i>=0; i--){
+            if(height[i] > max){
+                max = height[i];
+            }
+            suffix[i] = max;
+        }
+
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum += Math.min(prefix[i], suffix[i]) - height[i];
+        }
+
+        return sum;
+    }
+}
+
+// Time : O(N)
+// Space : O(N)
+
+
+// Approach 3
+// Two pointer
+
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int left = 0, right = n-1;
+        int sum=0;
+        int maxLeft=0, maxRight=0;
+        while(left<=right){
+
+            if(height[left] <= height[right]){
+
+                if(maxLeft <= height[left]){
+                    maxLeft = height[left];
+                }else{
+                    sum += maxLeft - height[left];
+                }
+
+                left ++;
+            }
+
+            else{
+
+                if(maxRight <= height[right]){
+                    maxRight = height[right];
+                }else{
+                    sum += maxRight - height[right];
+                }
+
+                right --;
+            }
+        }
+        return sum;
+    }
+}
+
+// Time : O(N)
+// Space : O(1)
