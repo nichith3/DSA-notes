f6b547cde11e30d9815473ff73ce65164625fea3
nichith3 <154726332+nichith3@users.noreply.github.com>
2024-05-24 10:55:53 +0530

    Update 142. Linked List Cycle II.java

--- Linked List/142. Linked List Cycle II.java
+++ Linked List/142. Linked List Cycle II.java
@@ -1,14 +1,38 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
+// Approach 1
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        boolean hasCycle = false;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+                hasCycle = true;
+                break;
+            }
+        }
+        if(!hasCycle){
+            return null;
+        }
+        ListNode temp = head;
+        while(temp!=slow){
+            slow = slow.next;
+            if(slow == fast){
+                temp = temp.next;
+            }
+        }
+        return temp;
+    }
+}
+
+// Time : O(N*M) --> M is less than length of LL
+// Space : O(1)
+
+
+// Approach 2
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head;
@@ -51,3 +75,31 @@ public class Solution {
         return length;
     }
 }
+
+// Time : O(2N)
+// Space : O(1)
+
+
+// Approach 3
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+    ListNode slow = head, fast = head;
+
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (slow == fast) {
+        while (head != slow) {
+          head = head.next;
+          slow = slow.next;
+        }
+        return slow;
+      }
+    }
+    return null;
+    }
+}
+// Time : O(N)
+// Space : O(1)
